034ac8909030c0a19e044b12a5e5b7337633a4a8
AlexiaTdev <alexia.teissier1@epsi.fr>
2022-03-20 19:42:27 +0100

    write LED with mask on PORTB

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,29 @@
 #include <Arduino.h>
 #define LED 13
+#define LED_MASK (1<<5)
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(LED, OUTPUT);
+  led_setup();
   Serial.println("blink");
 }
 
 void led_setup() {
-  //utilisation du code arduino
-  digitalWrite(LED, HIGH);
+  //je ne veux pas utiliser les fonctions arduino
+  led_on();
+}
+
+ void led_on(){
+  PORTB |= LED_MASK;
+}
+void led_off(){
+  PORTB &= ~LED_MASK;
 }
 
 void loop() {
-  led_setup();
+  // put your main code here, to run repeatedly:
+  led_on();
   delay(1000);
+  led_off();
 }
