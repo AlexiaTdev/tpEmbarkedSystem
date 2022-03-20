58158c2201aaee907f9f7e1ccb4cf1c5e84a72b6
AlexiaTdev <alexia.teissier1@epsi.fr>
2022-03-20 19:40:39 +0100

    write LED with arduino code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#define LED 13
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  Serial.println("blink");
+}
+
+void led_setup() {
+  //utilisation du code arduino
+  digitalWrite(LED, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  led_setup();
+  delay(1000);
 }
