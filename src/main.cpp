9b3e550c05553a19dfa116fb4f1062c42589a14c
AlexiaTdev <alexia.teissier1@epsi.fr>
2022-03-20 19:47:46 +0100

    configure DHT22 for use with registers

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,29 @@
 #include <Arduino.h>
-#define LED 13
-#define LED_MASK (1<<5)
-#define CS_PRESCALER_1024 ((1<<CS10) | (1<<CS12))
-#define TICKS_IN_SECOND 15625
-void led_setup() {
-  //je ne veux pas utiliser les fonctions arduino
-  DDRB |= LED_MASK;
-
+//dht22 sert à alumer le atm
+void dht22_send_start() {
+    DDRB |= (1<<DDB0);   // je set arduino  en état sortie sur le port DDRB
+    PORTB &= ~(1<<DDB0); // je mets la val 0 sur le port 0 de DDRB
+    delay(2000);
+    PORTB |= (1 << DDB0); // je mets la val 1 sur le port 0 de DDRB
+    delayMicroseconds(30);
+    DDRB &= ~(1<<DDB0); //je remets l'arduino en écoute
 }
-void clock_setup() {
-    //clock prescaler
-  TCCR1A = 0; //WGM11 et WGM1O à 0
-    //mode
-  //TCCR1B = (1<<0) | (1<<2) | (1<<3); //WGM13 à 0, WGM12 à 1, CS1à à 1, CS11 à 0, CS12 à 1
-  //la meme   TCCR1A = (1<<CS10) | (1<<CS12) | (1<<WGM12); 
-  //la meme   TCCR1A = CS_PRESCALER_1024 | (1<<WGM12); 
-  TCCR1B = CS_PRESCALER_1024 | (1<<WGM12); 
-    // valeur a comparer
-  // je prends 15625 je le mets en binaire  0B 0011 1101 0000 1001 . Je voudrais ne garder que 0000 1001 pour le low.
-  //il faut le caster en Uint8_t ça récupère que la partie à droite
-  //OCR1AL = (uint8_t) (TICKS_IN_SECOND & 0xFF);
-  //OCR1AH = (uint8_t) (TICKS_IN_SECOND & (0xFF << 8)) >>8;    la meme   OCR1AH = (uint8_t) (TICKS_IN_SECOND & 0xFF00);
-  OCR1A = TICKS_IN_SECOND;
-    //interrupt
-  TIMSK1 = (1 << OCIE1A);
+
+void dht22_receive_ack() {
+  //PINB & (1<<PINB0) en gros PINB ça va rendre toutes les valeur de chaque bit/ le & c'est une opération bitwise /
+  // (1<<PINB0) on mets un 1 au niveau bit0
+    while(PINB & (1<<PINB0)) {
+      // Transmission des valeurs
+
+    }; 
 }
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  led_setup();
-  clock_setup();
-  Serial.println("blink");
-}
-
-void led_toggle(){
-  PINB = LED_MASK;
-}
-
-ISR(TIMER1_COMPA_vect) {
-  led_toggle();
+  dht22_send_start(); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //led_toggle();
-  delay(10000);
-  TCCR1B &= ~(CS_PRESCALER_1024);
-  delay(10000);
-  TCCR1B |= (CS_PRESCALER_1024);
+  dht22_receive_ack();
 }
